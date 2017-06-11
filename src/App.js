8db4b0323a6d71a7519473464d50562901b1aec1
Poh Peng <wangpp1@hotmail.com>
2017-06-11 15:57:22 +0800

    set up sample states

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 import TopicList from './TopicList';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      list: [],
+    };
+  }
+
+  componentDidMount(){
+    this.setState({
+      list: [],
+    });
+  }
+
   render() {
+
+    const { list } = this.state;
+
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <TopicList/>
+        <div className='header'> Reddit Sample </div>
+        <div className='list'>
+          <TopicList list={list}/>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
--- src/Topic.js
+++ src/Topic.js
@@ -1,21 +1,33 @@
 import React, { Component } from 'react';
 
 export default class Topic extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
     this.state = {
+      id: props.key,
       upvotes: 0,
     };
   }
 
+  upvote(id){
+    console.log('up');
+  }
+
+  downvote(id){
+    console.log('down');
+  }
+
   render(){
-    const {upvotes} = this.state;
+    const {upvotes, id} = this.state;
     const {content} = this.props;
 
     return (
-      <div>
-        <h1>{content}</h1>
+      <div className='topic'>
+        <div className='upvotes'> Votes: {upvotes}</div>
+        <div className='content'>{content}</div>
+        <button onClick={this.upvote.bind(this, id)} className='up'> Upvote</button>
+        <button onClick={this.downvote.bind(this, id)} className='down'> Downvote</button>
       </div>
     );
   }
